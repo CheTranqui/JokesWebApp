6590cc9a9990390bbdb64d6599f61749978e5f15
Che Tranqui <chaz.peterson1@gmail.com>
2021-03-13 12:02:11 -0600

    search implemented
    search implemented

---
+++ Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        // auto-gen properties in C# with "prop"
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            // auto-gen constructor in C# with "ctor"
+            // leaving code in this constructor empty
+        }
+    }
+}
