66243be6ff8366cac278eb2fc70c8cfc61947e5b
Che Tranqui <chaz.peterson1@gmail.com>
2021-03-23 13:57:05 -0500

    final state
    completed the tutorial - includes:
    1.  changing background and color
    2.  adding login requirement (w/o email confirmation)
    3.  adding razor-based elementID and modifying element in JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleAnswer(Id) {
+    event.preventDefault();
+    let elementId = Id + " Answer";
+    let element = document.getElementById(elementId).firstElementChild;
+    if (element.style.display != "inline-block") {
+        element.style.display = "inline-block";
+        element.nextElementSibling.innerText = "\nHide Answer";
+    }
+    else {
+        element.style.display = "none";
+        element.nextElementSibling.innerText = "Show Answer";
+    }
+}
